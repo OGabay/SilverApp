98ac24f5b74ac26520a56bcc4d411ccfc0356c26
OGabay <111686955+OGabay@users.noreply.github.com>
2022-09-04 19:11:56 +0300

    fdfdf

---
+++ bot.js
@@ -0,0 +1,13 @@
+const {Telegraf} = require('telegraf');
+const axios = require('axios');
+
+const bot = new Telegraf('5494838007:AAHFR1VYSQn6_lKVsj2EXreSXR-XH2kb098');
+var welcomeMessage;
+
+bot.command("start", ctx =>
+{
+    
+})
+
+
+bot.launch();
